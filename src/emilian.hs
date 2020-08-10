232c8c34c96d73a699dd24d584af3a40e3e00173
Liam Wynn <wynnliam@pdx.edu>
2020-08-10 10:51:05 -0700

    Added definitions for State, Symbol, Alphabet

--- src/emilian.hs
+++ src/emilian.hs
@@ -1,1 +1,12 @@
-main = putStrLn "Hello"
+data State = State String deriving (Show)
+data Symbol = Symbol Char deriving (Show)
+data Alphabet = Alphabet [Symbol] deriving (Show)
+
+constructState :: String -> State
+constructState name = State name
+
+constructAlphabet :: [Char] -> Alphabet
+constructAlphabet chars = Alphabet (map Symbol chars)
+
+main = let alpha = constructAlphabet ['a', 'b', 'c'] in
+  putStrLn (show alpha)
