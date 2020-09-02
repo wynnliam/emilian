2acf56f0c97d4ddde744c79f4d76612b942e8db9
Liam Wynn <wynnliam@pdx.edu>
2020-09-02 10:31:27 -0700

    Added definition of eClosure

--- src/emilian.hs
+++ src/emilian.hs
@@ -1,3 +1,5 @@
+-- Liam Wynn, 9/2/2020, Emilian
+
 data State = State String deriving (Show, Eq)
 data Symbol = Symbol Char | Epsilon deriving (Show, Eq)
 data Alphabet = Alphabet [Symbol] deriving (Show)
@@ -15,16 +17,31 @@ move (Transition a s r) b t
   | a == b && s == t = r
   | otherwise = []
 
-main = do
-  let q0 = constructState "q0"
-  let q1 = constructState "q1"
-  let q2 = constructState "q2"
-  let s = Symbol 'a'
-  let s2 = Symbol 'b'
-  let t = Transition s q0 [q1, q2]
+-- From a given a set of states, we want to find every possible state you
+-- can reach from Epsilon transitions alone.
+eClosure :: [State] -> [Transition] -> [State]
+eClosure states transitions = error "Not yet implemented"
 
-  putStrLn (show (move t s q0))
-  putStrLn (show (move t s2 q0))
-  putStrLn (show (move t s q1))
-  putStrLn (show (move t s2 q1))
-  putStrLn (show (Epsilon))
+main = do
+  -- Defines the NFDA from page 116 of "Compilers: Principles, Tools, and Techniques"
+  -- First, define the alphabet. For now, this is a symbolic step, since we
+  -- never actually use this variable
+  let alpha = constructAlphabet ['a', 'b']
+
+  let q0 = constructState "0"
+  let q1 = constructState "1"
+  let q2 = constructState "2"
+  let q3 = constructState "3"
+  let q4 = constructState "4"
+
+  -- Now, we define our transitions
+  let q0_q1 = Transition Epsilon q0 [q1]
+  let q0_q3 = Transition Epsilon q0 [q3]
+  let q1_q2 = Transition (Symbol 'a') q1 [q2]
+  let q2_q2 = Transition (Symbol 'a') q2 [q2]
+  let q3_q4 = Transition (Symbol 'b') q3 [q4]
+  let q4_q4 = Transition (Symbol 'b') q4 [q4]
+  -- For ease of use, let's put these in a list
+  let transitions = [q0_q1, q0_q3, q1_q2, q2_q2, q3_q4, q4_q4]
+
+  putStrLn (show (eClosure [q0] transitions))
