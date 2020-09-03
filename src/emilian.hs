9bb8f19e08078f77a157548d2fcbc784966fc4ed
Liam Wynn <wynnliam@pdx.edu>
2020-09-02 20:30:37 -0700

    Cleaned up main

--- src/emilian.hs
+++ src/emilian.hs
@@ -76,37 +76,4 @@ testNdfa =
       s9_to_s10]
 
 main = do
-  -- Defines the NDFA on page 120 of "Compilers: Principles, Tools, and Techniques"
-  let s0 = State "0"
-  let s1 = State "1"
-  let s2 = State "2"
-  let s3 = State "3"
-  let s4 = State "4"
-  let s5 = State "5"
-  let s6 = State "6"
-  let s7 = State "7"
-  let s8 = State "8"
-  let s9 = State "9"
-  let s10 = State "10"
-
-  -- Now define the transitons
-  let s0_to_s1 = Transition Epsilon s0 [s1]
-  let s0_to_s7 = Transition Epsilon s0 [s7]
-  let s1_to_s2 = Transition Epsilon s1 [s2]
-  let s1_to_s4 = Transition Epsilon s1 [s4]
-  let s2_to_s3 = Transition (Symbol 'a') s2 [s3]
-  let s3_to_s6 = Transition Epsilon s3 [s6]
-  let s4_to_s5 = Transition (Symbol 'b') s4 [s5]
-  let s5_to_s6 = Transition Epsilon s5 [s6]
-  let s6_to_s1 = Transition Epsilon s6 [s1]
-  let s6_to_s7 = Transition Epsilon s6 [s7]
-  let s7_to_s8 = Transition (Symbol 'a') s7 [s8]
-  let s8_to_s9 = Transition (Symbol 'b') s8 [s9]
-  let s9_to_s10 = Transition (Symbol 'b') s9 [s10]
-
-  let ndfa = [s0_to_s1, s0_to_s7, s1_to_s2, s1_to_s4,
-              s2_to_s3, s3_to_s6, s4_to_s5, s5_to_s6,
-              s6_to_s1, s6_to_s7, s7_to_s8, s8_to_s9,
-              s9_to_s10]
-
-  putStrLn (show (eClosure [s5] ndfa))
+  putStrLn (show (eClosure [(State "5")] testNdfa))
